16e832933ae0206ef321beacde56359efabc1778
Dom-Napoli <Dom-Napoli@users.noreply.github.com>
2023-10-29 13:47:23 -0700

    created new folder (wandertarot) in ./assets/images - includes all photo jpgs that I made.

--- assets/imgdata.js
+++ assets/imgdata.js
@@ -1,10 +1,12 @@
 const imgCard = [
-    { card_id: 0, img_url: "https://www.biddytarot.com/wp-content/uploads/2018/02/0-fool-345x480.png"}, 
-    { card_id: 0, img_url: "https://images.squarespace-cdn.com/content/v1/5575e56de4b08f79780bce2c/1627664171248-V6KRYVNRR4TYD8HE8ECS/The+Fool+Tarot+Card+%230.jpg?format=300w" },
-    { card_id: 1, img_url: "https://www.biddytarot.com/wp-content/uploads/2018/02/01-magician-345x480.png" }, 
-    { card_id: 1, img_url: "https://images.squarespace-cdn.com/content/v1/575c175722482ea13706dd8a/1660323695573-C0RR4FXL62LL21HZZJIY/Majors%2B01%2BThe%2BMagician.jpg?format=1000w" }, 
-    { card_id: 2, img_url: "https://www.biddytarot.com/wp-content/uploads/2018/02/0-fool-345x480.png"}, 
-    { card_id: 2, img_url: "https://www.biddytarot.com/wp-content/uploads/2018/02/0-fool-345x480.png"}, 
+    { card_id: 0, image: "./assets/wandertarot/images/fool.jpg" },
+
+    // { card_id: 0, img_url: "https://www.biddytarot.com/wp-content/uploads/2018/02/0-fool-345x480.png"}, 
+    // { card_id: 0, img_url: "https://images.squarespace-cdn.com/content/v1/5575e56de4b08f79780bce2c/1627664171248-V6KRYVNRR4TYD8HE8ECS/The+Fool+Tarot+Card+%230.jpg?format=300w" },
+    // { card_id: 1, img_url: "https://www.biddytarot.com/wp-content/uploads/2018/02/01-magician-345x480.png" }, 
+    // { card_id: 1, img_url: "https://images.squarespace-cdn.com/content/v1/575c175722482ea13706dd8a/1660323695573-C0RR4FXL62LL21HZZJIY/Majors%2B01%2BThe%2BMagician.jpg?format=1000w" }, 
+    // { card_id: 2, img_url: "https://www.biddytarot.com/wp-content/uploads/2018/02/0-fool-345x480.png"}, 
+    // { card_id: 2, img_url: "https://www.biddytarot.com/wp-content/uploads/2018/02/0-fool-345x480.png"}, 
 
 
 ]
